40dcfdffef7cadfcfcc41955cf2f45682421fa56
jaqueshere <jaques.gillis@gmail.com>
2015-12-20 23:29:25 -0500

    Rewrote quiz, and replaced background image

--- js/scripts.js
+++ js/scripts.js
@@ -52,7 +52,7 @@ var newQuiz = {
 	start: function() {
 		$('.options-overlay').css("visibility", "hidden");
 		$('.buttons').css("visibility", "hidden");
-		$('#message').text('This is a brief test to determine your basic reasoning ability. For each of the questions that follow, choose the option that is most reasonable. Click anywhere to proceed.');
+		$('#message').text('This is a brief quiz on some key points of early Andean art. The questions cover the Early Horizon to Late Horizon periods. Click anywhere to proceed.');
 		$('.welcome').click(function() {
 			$('.welcome').css("visibility", "hidden");
 			$('.buttons').css("visibility", "visible");
@@ -84,8 +84,8 @@ var newQuiz = {
 	}
 };
 
-var questions = [["There is a fork in the road. Down one path is the city of liars. Down the other is the city of truth tellers. An old man comes down one path. What question should you ask him to find out if he's a liar?",["Is Pepsi better than Coke?", "If you were a liar, what town would you say you came from?", "Are you a tree frog?", "Any of the above"], 3],
-	["If every dinosaur is an animal, and  every animal believes in causation: ",["The dinosaurs died out because of a meteor.", "The dinosaurs died out because of a volcano.", "The dinosaurs died out from smoking cigarettes.", "The dinosaurs could not have understood Bob Dylan."], 3],	
-	["The purpose of socks is: ",["to keep feet warm.", "to make you taller.", "to give you something to fold at the laundromat.", "to entertain dogs."], 2],
-	["If Sam can never find his keys after a night at Johny Brenda's wine bar, and on a Tuesday night Leslie finds Sam's keys:",["Leslie did not go to Johny Brenda's.", "Leslie kept the keys in her purse Tuesday.", "Leslie has studied quantum mechanics.", "Leslie knows Kung Fu."], 3],
-	["If a girl asks a guy what his sign is, there is a 1/11 probability that his sign is Scorpio. If she tells him her sign is Scorpio and asks him what his sign is, the probability his sign is Scorpio is: ",["1/11", "1/12", "1/5", "1/3"], 3]];
+var questions = [["Which of the following cultures was the earliest to export its religious imagery to neighboring cultures?",["Inca", "Chimu", "Tiwanaku", "Chavin"], 3],
+	["Which period of Nasca ceramics is typified by naturalistic, three-dimensional figures?",["Early", "Middle", "Late", "Devonian"], 0],	
+	["Which of these cultures is known for producing highly individualized portraiture in its ceramics?",["Paracas", "Nasca", "Moche", "Wari"], 2],
+	["Which of these cultures deliberately introduced small irregularities in otherwise perfectly repeated textile patterns?",["Paracas", "Nasca", "Wari", "Inca"], 2],
+	["Which culture's stonework did the Incas look to for inspiration?",["Nasca", "Wari", "Lambayeque", "Tiwanaku"], 3]];
